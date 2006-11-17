4f8405dc8d769960f0afb0e8ef793604f1a120d3
wolfram <wolfram@492e2ef8-fd0b-0410-a9f9-cf8e32e04608>
2006-11-17 20:58:09 +0000

    Big refactoring

---
+++ trunk/arbaro/src/net/sourceforge/arbaro/tree/Stem.java
@@ -0,0 +1,110 @@
+package net.sourceforge.arbaro.tree;
+
+import net.sourceforge.arbaro.transformation.Transformation;
+import net.sourceforge.arbaro.transformation.Vector;
+
+public interface Stem {
+
+	public abstract Vector getMinPoint();
+
+	public abstract Vector getMaxPoint();
+
+	/**
+	 * For debugging:
+	 * Prints out the transformation to stderr nicely 
+	 * (only if debugging is enabled)
+	 * 
+	 * @param where The position in the tree, i.e. wich stem
+	 *              has this transformation
+	 * @param trf  The transformation
+	 */
+	/*	
+	 void TRF(String where, Transformation trf) {
+	 DBG(where + ": " + trf.toString());
+	 }
+	 */
+	/**
+	 * Output debug string, when debugging ist enabled
+	 * 
+	 * @param dbgstr The output string
+	 */
+	/*
+	 public void DBG(String dbgstr) {
+	 // print debug string to stderr if debugging is enabled
+	 if (par.debug) System.err.println(getTreePosition() + ":" + dbgstr);
+	 }
+	 */
+	/**
+	 * The position of the stem in the tree. 0.1c2.3 means:
+	 * fourth twig of the third clone of the second branch growing
+	 * out of the first (only?) trunk 
+	 * 
+	 * @return The stem position in the tree as a string
+	 */
+	public abstract String getTreePosition();
+
+	public abstract double getLength();
+	
+	public abstract double getBaseRadius();
+	public abstract double getPeakRadius();
+	
+	public abstract int getLevel();
+
+	/**
+	 * Adds the current stem to the mesh object
+	 *  
+	 * @param mesh the mesh object
+	 * @throws Exception
+	 */
+	/*
+	 // TODO should be obsolete, when TreeTraversals are working
+	 void addToMesh(Mesh mesh, boolean withSubstems, boolean useQuads) throws Exception {
+	 
+	 if (par.verbose) {
+	 if (stemlevel<=1 && cloneIndex.size()==0) System.err.print(".");
+	 }
+	 
+	 //String indent = "    ";
+	 
+	 // create mesh part for myself
+	 if (segments.size()>0) {
+	 MeshPart meshpart = new MeshPart(this,stemlevel<=par.smooth_mesh_level, useQuads);
+	 for (int i=0; i<segments.size(); i++) {
+	 ((Segment)segments.elementAt(i)).addToMeshpart(meshpart);
+	 }
+	 mesh.addMeshpart(meshpart);
+	 }
+	 
+	 if (withSubstems) {
+	 // add clones to the mesh
+	 if (clones != null) {
+	 for (int i=0; i<clones.size(); i++) {
+	 ((Stem)clones.elementAt(i)).addToMesh(mesh,withSubstems, useQuads);
+	 }
+	 }
+	 
+	 if (substems != null) {
+	 tree.getProgress().incProgress(substems.size());
+	 
+	 for (int i=0; i<substems.size(); i++) {
+	 ((Stem)substems.elementAt(i)).addToMesh(mesh,withSubstems, useQuads);
+	 }
+	 }
+	 }
+	 }
+	 */
+
+	public abstract boolean traverseTree(TreeTraversal traversal)
+			throws TraversalException;
+
+	public abstract boolean traverseStem(StemTraversal traversal)
+			throws TraversalException;
+
+	// use with TreeTraversal
+	public abstract long getLeafCount();
+
+	public abstract boolean isClone();
+
+	public Transformation getTransformation();
+
+}
