c74ff0e47c118e1a460e14cc66008df71f1140c6
wolfram <wolfram@492e2ef8-fd0b-0410-a9f9-cf8e32e04608>
2006-12-19 12:19:55 +0000

    better exception handling, shielding generators and exporters with exception handling facades

--- trunk/arbaro/src/net/sourceforge/arbaro/tree/TreeGeneratorImpl.java
+++ trunk/arbaro/src/net/sourceforge/arbaro/tree/TreeGeneratorImpl.java
@@ -27,17 +27,19 @@ import java.io.PrintWriter;
 
 import net.sourceforge.arbaro.export.Progress;
 import net.sourceforge.arbaro.params.AbstractParam;
-import net.sourceforge.arbaro.params.ParamException;
 import net.sourceforge.arbaro.params.Params;
 
 /**
  * @author wolfram
  *
  */
-public class TreeGenerator {
+class TreeGeneratorImpl implements TreeGenerator {
 	Params params;
 
-	public Tree makeTree(Progress progress) throws Exception {
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#makeTree(net.sourceforge.arbaro.export.Progress)
+	 */
+	public Tree makeTree(Progress progress) {
 		TreeImpl tree = new TreeImpl(seed, params);
 		tree.make(progress);
 		
@@ -46,80 +48,94 @@ public class TreeGenerator {
 	
 	private int seed = 13;
 
-	public TreeGenerator() {
+	public TreeGeneratorImpl() {
 		params = new Params();
 	}
 	
-	public TreeGenerator(Params params) {
+	public TreeGeneratorImpl(Params params) {
 		this.params = params;
 	}
 
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#setSeed(int)
+	 */
 	public void setSeed(int seed) {
 		this.seed = seed;
 	}
 	
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#getSeed()
+	 */
 	public int getSeed() {
 		return seed;
 	}
 	
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#getParams()
+	 */
 	public Params getParams() {
 		return params;
 	}
 	
 	
-	public void setParam(String param, String value) throws ParamException {
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#setParam(java.lang.String, java.lang.String)
+	 */
+	public void setParam(String param, String value) {
 		params.setParam(param,value);
 	}
 	
+	// TODO: not used at the moment, may be the GUI
+	// should get a TreeGenerator as a ParamContainer
+	// and tree maker, and not work directly with Params
+	// class
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#getParam(java.lang.String)
+	 */
 	public AbstractParam getParam(String param) {
 		return params.getParam(param);
 	}
 	
-	/**
-	 * Returns a parameter group
-	 * 
-	 * @param level The branch level (0..3)
-	 * @param group The parameter group name
-	 * @return A hash table with the parameters
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#getParamGroup(int, java.lang.String)
 	 */
+	// TODO: not used at the moment, may be the GUI
+	// should get a TreeGenerator as a ParamContainer
+	// and tree maker, and not work directly with Params
+	// class
 	public java.util.TreeMap getParamGroup(int level, String group) {
 		return params.getParamGroup(level,group);
 	}
 
-	/**
-	 * Writes out the parameters to an XML definition file
-	 * 
-	 * @param out The output stream
-	 * @throws ParamException
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#writeParamsToXML(java.io.PrintWriter)
 	 */
-	public void writeParamsToXML(PrintWriter out) throws ParamException {
+	// TODO: not used at the moment, may be the GUI
+	// should get a TreeGenerator as a ParamContainer
+	// and tree maker, and not work directly with Params
+	// class
+	public void writeParamsToXML(PrintWriter out) {
 		params.toXML(out);
 	}
 	
-	/**
-	 * Clear all parameter values of the tree.
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#clearParams()
 	 */
 	public void clearParams() {
 		params.clearParams();
 	}
 	
-	/**
-	 * Read parameter values from an XML definition file
-	 * 
-	 * @param is The input XML stream
-	 * @throws ParamException
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#readParamsFromXML(java.io.InputStream)
 	 */
-	public void readParamsFromXML(InputStream is) throws ParamException {
+	public void readParamsFromXML(InputStream is) {
 		params.readFromXML(is);
 	}
 	
-	/**
-	 * Read parameter values from an Config style definition file
-	 * 
-	 * @param is The input text stream
-	 * @throws ParamException
+	/* (non-Javadoc)
+	 * @see net.sourceforge.arbaro.tree.TreeGenerator#readParamsFromCfg(java.io.InputStream)
 	 */
-	public void readParamsFromCfg(InputStream is) throws Exception {
+	public void readParamsFromCfg(InputStream is) {
 		params.readFromCfg(is);
 	}
 
